dfdbe802c38b15f72e66226fca5c95bf3cc0f585
Lokitha V <148523771+LokithaV02@users.noreply.github.com>
2023-10-25 15:24:07 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,14 @@
+print(10*'test')
+print(2+3)
+name = 'youtube'
+print('my ' +name)
+num = [25,130,389,473]
+print(num)
+print(num[0])
+print(num[1:])
+print(num[-1])
+print(num[-4])
+
+names = ['test','dog','tour']
+mil = [names,num]
+print(mil)
